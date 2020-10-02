98e3437bef45d922c1fd2c7a5cec44d78da2c328
YuShichao <ee2666@163.com>
2020-10-02 20:22:39 +0800

    竞赛首页的完善

--- src/components/views/competitions/index.vue
+++ src/components/views/competitions/index.vue
@@ -1,63 +1,227 @@
 <template>
-<el-row >
-  <el-col :span="5" v-for="(o, index) in 4" :key="o" :offset="index > 0 ? 1 : 0">
-    <el-card :body-style="{ padding: '0px' }">
-      <img src="@/assets/example.png" class="image">
-      <div style="padding: 14px;">
-        <span>中国大学生数学建模竞赛</span>
-        <div class="bottom clearfix">
-          <time class="time">2020.09.01-2020.10.01</time>
-          <el-button type="text" class="button" @click="getDetail">查看详情</el-button>
-        </div>
-      </div>
-    </el-card>
-  </el-col>
-</el-row>
+  <div>
+    <el-row>
+      <el-col :span="6" :offset="1">
+        <!-- 这里坑点，用封装组件接收消息，要是用native作为后缀才可 -->
+        <el-input v-model="search.name" placeholder="请输入竞赛名称" @keyup.enter.native="getSearch()" clearable @clear="clearSearch()"/>
+      </el-col>
+      <el-col :span="2" >
+        <el-button  @click="getSearch()"  type="primary" icon="el-icon-search" plain >搜索</el-button>
+      </el-col>
+    </el-row>
+   
+    <div class="bo">
+      <el-row>
+        <el-col :span="2">
+          <div class="titlefont">竞赛级别：</div>
+        </el-col>
+        <el-col :span="2">
+          <el-button size="mini" round @click="level('国家级')"
+            >国家级</el-button
+          >
+        </el-col>
+        <el-col :span="2">
+          <el-button size="mini" round @click="level('省级')">省级</el-button>
+        </el-col>
+        <el-col :span="2">
+          <el-button size="mini" round @click="level('校级')">校级</el-button>
+        </el-col>
+        <el-col :span="2">
+          <el-button size="mini" round @click="level('院级')">院级</el-button>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="bo">
+      <el-row>
+        <el-col :span="2">
+          <div class="titlefont">竞赛类型：</div>
+        </el-col>
+        <el-col :span="2">
+          <el-button size="mini" round @click="type('团队赛')">团队赛</el-button >
+        </el-col>
+        <el-col :span="2">
+          <el-button size="mini" round @click="type('个人赛')" >个人赛</el-button >
+        </el-col>
+      </el-row>
+    </div>
+    <div class="bo">
+      <el-row>
+        <el-col :span="2">
+          <div class="titlefont" v-if="query.levelName != '' || query.typeName != ''" >已选标签：</div>
+        </el-col>
+        <el-col :span="2">
+          <el-tag v-if="query.levelName != ''" size="medium" type="success" closable @close="closeLevel">{{ query.levelName }}</el-tag>
+        </el-col>
+        <el-col :span="2">
+          <el-tag v-if="query.typeName != ''" size="medium" type="warning" closable @close="closeType">{{ query.typeName }}</el-tag>
+        </el-col>
+        <el-col :span="2">
+          <el-button v-if="query.levelName != '' || query.typeName != ''" size="mini" type="primary" round @click="clean()">清除标签</el-button>
+        </el-col>
+      </el-row>
+    </div>
+    <el-divider></el-divider>
+    <el-row>
+      <el-col
+        :span="5"
+        v-for="(item, index) in result.items"
+        :key="item.val"
+        :offset="index % 4 > 0 ? 1 : 0">
+        <br />
+        <el-card :body-style="{ padding: '0px' }">
+          <img :src="item.image" class="image" style="height:230px; width=100px;"/>
+          <div style="padding: 14px">
+            <span>{{ item.name }}</span>
+            <div class="bottom clearfix">
+              <time class="time">{{ item.startTime }}至{{ item.endTime }}</time>
+              <el-button type="text" class="button" @click="goDetail(item.id)">查看详情</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <br />
+    <el-row>
+      <el-col :span="8" :offset="8">
+        <el-pagination
+          background
+          layout="sizes, prev, pager, next"
+          :page-sizes="[4, 8, 12, 16]"
+          :page-count="result.pages"
+          :page-size.sync="query.pageSize"
+          :current-page.sync="query.pageNo"
+          @current-change="getData()"
+          @size-change="getData()">
+        </el-pagination>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      currentDate: new Date()
-    };
+      //请求参数
+      query: {
+        pageSize: 8,
+        pageNo: 1,
+        name: "",
+        levelName: "",
+        typeName: "",
+        id: 4,
+      },
+      result: {
+        items: [],
+        pages: 0,
+      },
+      tag: {
+        levelswitch: false,
+        typeswitch: false,
+      },
+      search: {
+        name: ''
+      }
+    }
+  },
+  created() {
+    this.getData()
   },
   methods: {
-    getDetail() {
-      
-      // this.$router.replace('/detail')
+    goDetail(id) {
+      this.$router.push({
+        path: "/detail",
+        name: "Detail",
+        params: {id: id}
+      })
+    },
+    getSearch() {
+      this.query.name = this.search.name
+      this.query.pageNo = 1
+      this.getData()
+    },
+    clearSearch() {
+      this.query.name = ""
+      this.query.pageNo = 1
+      this.getData()
+    },
+    getData() {
+      this.axios.post("/competition/list",response => {
+          this.result.items = response.data.records
+          this.result.pages = response.data.pages
+          this.query.pageSize = response.data.size
+          this.query.pageNo = response.data.current
+        },
+        this.query
+      )
+    },
+    time(sort) {
+      //按时间排序，0，1，2，3  4为不排序
+      this.query.id = sort
+      this.getData()
+    },
+    level(levelname) {
+      this.query.levelName = levelname
+      this.getData()
+    },
+    type(typename) {
+      this.query.typeName = typename
+      this.getData()
+    },
+    closeLevel() {
+      this.query.levelName = ""
+      this.getData()
+    },
+    closeType() {
+      this.query.typeName = ""
+      this.getData()
+    },
+    clean() {
+      this.query.name = ""
+      this.query.levelName = ""
+      this.query.typeName = ""
+      this.query.id = 4
+      this.getData()
     }
   }
 }
 </script>
 <style scoped>
-   .time {
-    font-size: 13px;
-    color: #999;
-  }
-  
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
+.time {
+  font-size: 13px;
+  color: #999;
+}
 
-  .button {
-    padding: 0;
-    float: right;
-  }
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
 
-  .image {
-    width: 100%;
-    display: block;
-  }
+.button {
+  padding: 0;
+  float: right;
+}
 
-  .clearfix:before,
-  .clearfix:after {
-      display: table;
-      content: "";
-  }
-  
-  .clearfix:after {
-      clear: both
-  }
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
+.titlefont {
+  font-size: 15px;
+  text-align: center;
+  margin-top: 4px;
+}
+.bo {
+  margin: 15px;
+}
 </style>
--- src/components/views/detail-competition/index.vue
+++ src/components/views/detail-competition/index.vue
@@ -1,40 +1,41 @@
 <template>
 <div>
     <el-row>
-        <el-col :span="8" >
+        <el-col :span="7" >
            <el-button round @click="back"><i class="el-icon-arrow-left el-icon--left"></i>返回</el-button>
         </el-col>
-        <el-col :span="6" :offset="1">
-           <div class="title">中国计算机设计大赛</div>
+        <el-col :span="8" :offset="1">
+           <div class="title">{{ item.name }}</div>
         </el-col>
-        <el-col :span="1" :offset="6">
+        <el-col :span="1" :offset="4">
            <el-button type="primary" round @click="submmit">立即参赛</el-button>
         </el-col>
     </el-row>
-    <el-divider></el-divider>
+     <el-divider content-position="left">简要介绍</el-divider>
     <el-row>
-        <el-col :span="5" :offset="1">
-            <img src="@/assets/example.png" style="width: 300px; height: 300px" class="image">
+        <el-col :span="4" :offset="4">
+            <img :src="item.image" style="width: 280px; height: 320px" class="image">
         </el-col>
-        <el-col :span="16" :offset="2"> 
+        <el-col :span="10" :offset="2"> 
             <div class="texts">
-            <div>竞赛类型：一类竞赛</div>
-            <el-divider></el-divider>
-            <div>竞赛级别：国家级</div>
-            <el-divider></el-divider>
-            <div>举办单位：教育部计算机委员会</div>
-            <el-divider></el-divider>
-            <div>开始时间：2020年9月1日</div>
-            <el-divider></el-divider>
-            <div>结束时间：2020年10月1日</div>
-            <el-divider></el-divider>
+            <el-divider content-position="left">竞赛类型</el-divider>
+            <div>{{item.typeName}}</div>
+            <el-divider content-position="left">竞赛级别</el-divider>
+            <div>{{item.levelName}}</div>
+            <el-divider content-position="left">开始时间</el-divider>
+            <div>{{item.startTime}}</div>
+            <el-divider content-position="left">结束时间</el-divider>
+            <div>{{item.endTime}}</div>
+    
             </div>
         </el-col>
     </el-row>
+    <el-divider content-position="left">详细介绍</el-divider>
     <el-row>
-        <el-col :span="16" :offset="1"> 
-        详细介绍：
-       
+        <el-col :span="16" :offset="4"> 
+            <div class="detail">
+        {{item.detail}}
+       </div>
         </el-col>
     </el-row>
 
@@ -45,15 +46,27 @@
 export default {
   data() {
     return {
-      
+      item: {
+          name:'',
+          startTime:'',
+          endTime:'',
+      },
+      id: this.$route.params.id
     }
+  },
+  created() {
+     this.axios.get('/competition/getById',res=>{
+         this.item = res.data[0]
+     },
+     { id: this.id }
+     )
   },
   methods: {
     back() {
-       //this.$router.replace('/competitions')
+       this.$router.push('/competitions')
     }, 
     submmit() {
-       // this.$router.replace('/form')
+        this.$router.replace('/form')
     }
   }
 }
@@ -67,6 +80,10 @@ export default {
  .texts {
      font-family: "微软雅黑";
      font-size: 20px;
-     
+     text-align: center;
+ }
+ .detail{
+      font-family: "微软雅黑";
+     font-size: 20px;
  }
 </style>
